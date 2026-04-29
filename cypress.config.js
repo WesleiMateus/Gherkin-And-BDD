c9ae3b2f4aa979c1cb203d90e73764154fa672fe
Weslei Mateus <wesleyhprozao@gmail.com>
2026-04-29 01:29:16 -0400

    adding report on project ( mochawesome )

--- cypress.config.js
+++ cypress.config.js
@@ -8,15 +8,21 @@ const {
 } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
+  projectId: 'icyu2z',
   allowCypressEnv: true,
 
   e2e: {
     specPattern: "**/*.feature",
     baseUrl: "https://automationpratice.com.br/",
     defaultCommandTimeout: 3000,
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      charts: true,
+      reportTitle: "Testing using BDD, Gherkin and Cucumber",
+    },
     setupNodeEvents(on, config) {
       addCucumberPreprocessorPlugin(on, config);
-
+      require("cypress-mochawesome-reporter/plugin")(on);
       on(
         "file:preprocessor",
         createBundler({
